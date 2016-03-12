da3b8ec7505ffd6dafcecd4adbb5997eb55b9190
SamTebbs33 <samuel.tebbs@yahoo.com>
2016-03-12 22:02:59 +0000

    Added comments to the functions I made

---
+++ src/bh4/Symbol.java
@@ -0,0 +1,14 @@
+package bh4;
+
+import java.util.HashMap;
+
+/**
+ * Created by samtebbs on 12/03/2016.
+ */
+public class Symbol {
+
+    private static HashMap<String, Symbol> symbols = new HashMap<>();
+
+
+
+}

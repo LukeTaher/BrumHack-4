f889bca3bc86cfbd05512bc74ce6c0d3a4d68748
Peran Truscott <darthperagon@gmail.com>
2016-03-13 06:14:07 +0000

    test

--- index.php
+++ index.php
@@ -343,8 +343,8 @@
               case 5:
                 opponentSrc = addToLastBlock(opponentSrc, "i = 0;");
           }
-          console.log(src);
           totalString = src.split("\n");
+          console.log(opponentSrc);
           theirString = opponentSrc.split("\n");
       }
 

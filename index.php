fe658e5641fd513fa1a92abba63a13a691a8084b
Luke Taher <Luke@Lukes-MacBook-Pro.local>
2016-03-13 09:20:57 +0000

    added compile stuff

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
         <div style="width:50%; top:0px;float:left;">
           <div id= "myName" onClick="this.contentEditable='false';" style="width:50%; top:0px;float;"><h2><?PHP echo $_GET['p1']; ?></h2></div>
           <div id="text" onClick="this.contentEditable='true';">lorem ipsum dolor lorem ipsum dolorlorem ipsum dolor</div>
-           <img id="compile" src="play.png" width=75, height=75, onClick="compile()"/>
+           <img id="compile" src="play.png" width=75, height=75, onClick="compile()" style="cursor: pointer;"/>
         </div>
         <div style="width:50%; top:0px;float:left;">
           <div id= "theirName"onClick="this.contentEditable='false';" style="width:50%; top:0px;float;"><h2><?PHP echo $_GET['p2']; ?><h2></div>
@@ -76,6 +76,22 @@
       var nyanShowing = false;
       var epShowing = true;
       var coveredCounter = 100;;
+
+      function compile()
+      {
+        alert("yup");
+        $.post("http://188.166.145.0/save.php", {raw : totalString.join("\n"), user: <?PHP echo $_GET['p1']; ?>}, function(data)
+        {
+            if(data == "true")
+            {
+                alert("You Win");
+            }
+            else
+            {
+                alert("Nope");
+            }
+        });
+      }
 
 
       function canvasClick(event)
